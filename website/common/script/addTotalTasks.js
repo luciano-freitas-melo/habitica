54fbbed5c2f879c52b0247ad3fba489c586b6df7
Madu01 <m4dud01@gmail.com>
2023-02-01 00:25:02 -0300

    Ciclo 2(TDD): criação do código da funcionalidade

--- website/common/script/addTotalTasks.js
+++ website/common/script/addTotalTasks.js
@@ -1,11 +1,16 @@
-export default function addTaskTotal (type) {
-  let totalTask = 0;
+let totalTask = 0;
+let totalHab = 0;
 
+export default function addTaskTotal (type) {
   if (type === 'task') {
     totalTask += 1;
     return totalTask;
   }
+
+  if (type === 'habit') {
+    totalHab += 1;
+    return totalHab;
+  }
+
   return 0;
 }
-
-addTaskTotal('task');
