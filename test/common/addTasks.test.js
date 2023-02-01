9ff598ab0462f5d782e760be1e9fa90a6381b82b
Madu01 <m4dud01@gmail.com>
2023-02-01 00:16:56 -0300

    Ciclo 2(TDD): criação do código do teste

--- test/common/addTasks.test.js
+++ test/common/addTasks.test.js
@@ -4,4 +4,12 @@ describe('contador de tarefas criadas', () => {
   it('deveria acrescentar mais 1 na contagem da task retornando 1', () => {
     expect(addTaskTotal('task')).to.equal(1);
   });
+
+  it('deveria acrescentar mais 1 na contagem do habit retornando 1', () => {
+    expect(addTaskTotal('habit')).to.equal(1);
+  });
+
+  it('deveria fazer a contagem do habit retornando 2, pois já foi chamado duas vezes', () => {
+    expect(addTaskTotal('habit')).to.equal(2);
+  });
 });
